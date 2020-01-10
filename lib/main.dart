b3b457b570caa5662d9bafb6c14b4f36643ab624
root <root@localhost.localdomain>
2020-01-10 11:19:37 +0600

    complete adding form

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
 }
 
 class Login extends StatelessWidget {
+  // final _formKey = GlobalKey<FormState>() ;
   const Login({Key key}) : super(key: key);
 
   @override
@@ -15,10 +16,70 @@ class Login extends StatelessWidget {
           title: Text('Log in'),
           centerTitle: true,
         ),
-        body: Container(
-          color: Colors.black,
+        body: LoginScreen(),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passField(),
+            submitButton(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email',
+      ),
+    );
+  }
+
+  Widget passField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () {
+        print('fuck ya!');
+      },
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
+        side: BorderSide(color: Colors.black),
+      ),
+      child: Text(
+        'Submit',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      color: Colors.blue,
+      padding: EdgeInsets.all(8.0),
     );
   }
 }
