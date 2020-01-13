1da18607b2fe510de418aff813b8144608ea7781
RomjanHossain <romjanhossain726526@gmail.com>
2020-01-13 10:10:36 +0600

    designed complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Login());
+  runApp(MaterialApp(
+    home: Login(),
+  ));
 }
 
 class Login extends StatelessWidget {
-  // final _formKey = GlobalKey<FormState>() ;
-  const Login({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Log in'),
-          centerTitle: true,
-        ),
-        body: LoginScreen(),
-      ),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
   final finalkey = GlobalKey<FormState>();
+  // const Login({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20.0),
-      child: Form(
-        key: finalkey,
-        child: Column(
-          children: <Widget>[
-            emailField(),
-            passField(),
-            Container(
-              margin: EdgeInsets.only(top: 7.6),
+    return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Colors.black,
+      //   title: Text('data'),
+      //   centerTitle: true,
+      // ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/boo.jpg"),
+            fit: BoxFit.cover,
+            alignment: Alignment(-0.5, 0.1),
+          ),
+        ),
+        child: Container(
+          margin: EdgeInsets.only(top: 70, bottom: 30, left: 40),
+          color: Colors.transparent,
+          child: Form(
+            key: finalkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              children: <Widget>[
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Trajan Pro',
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  height: 20,
+                ),
+                emailField(),
+                Container(
+                  height: 20,
+                ),
+                passField(),
+                Container(
+                  height: 20,
+                ),
+                submitButton(),
+              ],
             ),
-            submitButton(),
-          ],
+          ),
         ),
       ),
     );
@@ -51,20 +65,39 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget emailField() {
     return TextFormField(
+      cursorColor: Colors.white,
       validator: (String value) {
-        var c = value.contains('@');
-        var d = value.contains('.com');
+        var c = value.contains("@");
+        var d = value.contains(".com");
         var F = c && d;
         if (!F) {
           return 'please enter a valid email';
         }
-        ;
+      },
+      onSaved: (String value) {
+        print(value);
       },
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-        hintText: 'you@example.com',
-        labelText: 'Email',
-      ),
+          hintText: 'you@example.com',
+          labelText: 'Email',
+          icon: Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.white,
+          )),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.orange,
+          )),
+          border: OutlineInputBorder(),
+          labelStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          )),
     );
   }
 
@@ -72,20 +105,43 @@ class _LoginScreenState extends State<LoginScreen> {
     return TextFormField(
       validator: (String value) {
         if (value.length < 8) {
-          return 'please enter a valid email';
+          return 'pass should be > 7';
         }
         ;
       },
+      onSaved: (String value) {
+        print(value);
+      },
       obscureText: true,
-      decoration:
-          InputDecoration(labelText: 'Password', hintText: 'who cares!'),
+      decoration: InputDecoration(
+          labelText: 'Password',
+          hintText: 'who cares!',
+          icon: Icon(
+            Icons.lock,
+            color: Colors.white,
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.white,
+          )),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.orange,
+          )),
+          border: OutlineInputBorder(),
+          labelStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          )),
     );
   }
 
   Widget submitButton() {
     return RaisedButton(
       onPressed: () {
-        finalkey.currentState.validate();
+        if (finalkey.currentState.validate()) {
+          finalkey.currentState.save();
+        }
       },
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(18.0),
